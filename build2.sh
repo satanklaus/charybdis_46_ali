a98abc60810ff9a273b7d412b1a878e7248b34e9
saiba <saibatuth@mail.ru>
2026-02-12 08:07:01 +0300

    sdfa

--- build2.sh
+++ build2.sh
@@ -5,7 +5,7 @@ logging=" -S zmk-usb-logging"
 builddir=$PWD
 mkdir $PWD/artifacts &>/dev/null
 
-source ~/venvs/zmk2/bin/activate
+source ~/venvs/zmk/bin/activate
 
 cd ~/src/zmk/app
 
@@ -14,7 +14,7 @@ targets=(charybdis_left charybdis_right settings_reset)
 for target in ${targets[*]}; do 
 	echo $target; 
 	mkdir -p ./build/$target &>/dev/null
-	west build -b nice_nano_v2 -d build/$target ${logging:-""} -- -DSHIELD=$target -DZMK_CONFIG=$HOME/src/charybdis_46_ali/config -DZMK_EXTRA_MODULES="$HOME/src/zmk-pmw3610-driver/;$HOME/src/zmk-input-processor-keybind" &>/dev/null
+	west build -b nice_nano_v2 -d build/$target ${logging:-""} -- -DSHIELD=$target -DZMK_CONFIG=$HOME/src/charybdis_46_ali/config -DZMK_EXTRA_MODULES="$HOME/src/zmk-pmw3610-driver/;$HOME/src/zmk-input-processor-keybind"
 	echo 	BUILD STATUS: $?
 	cp ./build/$target/zephyr/zmk.uf2 $builddir/artifacts/$target.uf2
 done
